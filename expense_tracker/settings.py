d298d2f5c6edc077b360742788242853acc92e70
areti pavan <pavanareti02@gmail.com>
2021-03-19 20:50:30 +0530

    2nd

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django-heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,5 +125,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'expense_tracker/static')]
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-
+import django_heroku
 django_heroku.settings(locals())
